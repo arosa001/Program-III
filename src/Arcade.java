9612d43083f9ea886a36d6c1fe5f51e7400b14dc
arosa001 <arosa001@opendeusto.es>
2015-10-16 19:27:32 +0200

    Clases Basicas

---
+++ src/Arcade.java
@@ -0,0 +1,9 @@
+
+public class Arcade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/SuperManager.java
@@ -0,0 +1,9 @@
+
+public class SuperManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
